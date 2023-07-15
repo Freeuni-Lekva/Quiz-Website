739e2b357dafdffcd32dfea2311cdbe5ecd8b07b
nika <nkovz21@freeuni.edu.ge>
2023-07-15 11:24:50 +0400

    Fix some problems on #6 issue

--- QuizWebsite/src/main/java/dao/UsersDao.java
+++ QuizWebsite/src/main/java/dao/UsersDao.java
@@ -2,19 +2,14 @@ package dao;
 
 import models.Account;
 
-public class UsersDao implements Users {
-
-    @Override
+public class UsersDao {
     public void add(Account account) {
 
     }
-
-    @Override
     public String getPassword(String userName) {
         return null;
     }
 
-    @Override
     public Account getAccount(String userName) {
         return null;
     }
