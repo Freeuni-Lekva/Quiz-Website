30d30675aef4fa57e9e432c842b3f20d84d2fd6b
Nika Keshelava <nikkakeshelava@gmail.com>
2023-07-18 22:21:34 +0400

    add ConnectionManager class and create connection when ContextListener is initialized

--- QuizWebsite/src/main/java/dao/UsersDao.java
+++ QuizWebsite/src/main/java/dao/UsersDao.java
@@ -2,10 +2,21 @@ package dao;
 
 import models.Account;
 
+import java.sql.Connection;
+
 public class UsersDao {
+    private Connection conn;
+
+
+    public UsersDao(Connection conn) {
+        this.conn = conn;
+    }
+
+
     public void add(Account account) {
 
     }
+
     public String getPassword(String userName) {
         return null;
     }
--- QuizWebsite/src/main/java/listeners/ContextListener.java
+++ QuizWebsite/src/main/java/listeners/ContextListener.java
@@ -1,14 +1,24 @@
 package listeners;
 
+import dao.ConnectionManager;
 import dao.UsersDao;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        UsersDao users = new UsersDao();
+        Connection conn = null;
+        try {
+            conn = ConnectionManager.getConnection();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+        }
+        UsersDao users = new UsersDao(conn);
         servletContextEvent.getServletContext().setAttribute("users", users);
     }
 
