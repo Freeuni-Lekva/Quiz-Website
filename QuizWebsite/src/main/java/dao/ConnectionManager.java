2181b6628f9c97e96deb9f6b73553fbd81c5962f
Nika Keshelava <nikkakeshelava@gmail.com>
2023-07-19 11:39:42 +0400

    #10 change db_name

--- QuizWebsite/src/main/java/dao/ConnectionManager.java
+++ QuizWebsite/src/main/java/dao/ConnectionManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
     private static final String user = "root";
     private static final String password = "root";
-    private static final String db_name = "quiz_website";
+    private static final String db_name = "QuizWebsite";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
