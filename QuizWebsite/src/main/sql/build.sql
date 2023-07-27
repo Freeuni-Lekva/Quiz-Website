62f98f5a7a342542e449fdf7c55578049abd940a
nika <nkovz21@freeuni.edu.ge>
2023-07-27 21:05:05 +0400

    - Complete build.sql file for creation of users table

--- QuizWebsite/src/main/sql/build.sql
+++ QuizWebsite/src/main/sql/build.sql
@@ -10,3 +10,15 @@ CREATE TABLE friends
     user_id  INT,
     friend_id INT
 );
+
+-- Create the users table. Table has 5 columns, user_id is primary
+-- and auto incremented, username should be unique for each user
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) UNIQUE,
+    password VARCHAR(100),
+    user_type VARCHAR(100),
+    salt VARCHAR(100));
+
+ALTER TABLE users
+ADD CONSTRAINT unique_username UNIQUE (username);
