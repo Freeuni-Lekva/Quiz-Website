ddfbafbcd593b77ca385f7faac5aa183a04ee532
Nika Keshelava <nikkakeshelava@gmail.com>
2023-08-02 18:01:13 +0400

    #24 add messages table to sql file

--- QuizWebsite/src/main/sql/build.sql
+++ QuizWebsite/src/main/sql/build.sql
@@ -7,19 +7,31 @@ DROP TABLE IF EXISTS friends;
 -- second one - another user's id which is friend of this user
 CREATE TABLE friends
 (
-    user_id  INT,
+    user_id   INT,
     friend_id INT
+);
+
+CREATE TABLE messages
+(
+    id            int primary key auto_increment,
+    from_username VARCHAR(100),
+    to_username   VARCHAR(100),
+    message       VARCHAR(1000) not null,
+    create_date   timestamp     not null,
+    foreign key (from_username) references users (username) on delete cascade,
+    foreign key (to_username) references users (username) on delete cascade
 );
 
 -- Create the users table. Table has 5 columns, user_id is primary
 -- and auto incremented, username should be unique for each user
-CREATE TABLE IF NOT EXISTS users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) UNIQUE,
-    password VARCHAR(100),
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id   INT AUTO_INCREMENT PRIMARY KEY,
+    username  VARCHAR(100) UNIQUE,
+    password  VARCHAR(100),
     user_type VARCHAR(100),
-    salt VARCHAR(100)
+    salt      VARCHAR(100)
 );
 
 ALTER TABLE users
-ADD CONSTRAINT unique_username UNIQUE (username);
+    ADD CONSTRAINT unique_username UNIQUE (username);
