ecda9906d7eb9e79389568b4a94548c556f6a4ac
Nika Keshelava <nikkakeshelava@gmail.com>
2023-08-08 17:45:11 +0400

    #24 update the handling of exceptions in message, challenge and friendRequest classes

--- QuizWebsite/src/main/java/dao/ChallengeDao.java
+++ QuizWebsite/src/main/java/dao/ChallengeDao.java
@@ -16,39 +16,52 @@ public class ChallengeDao {
         this.conn = conn;
     }
 
-    public void createChallenge(Challenge ch) throws SQLException {
+    public void createChallenge(Challenge ch) {
         PreparedStatement ps;
-        ps = this.conn.prepareStatement("INSERT INTO challenges(from_username, to_username, quiz_id) " +
-                "VALUES(?, ?, ?)");
-        ps.setString(1, ch.getFromUsername());
-        ps.setString(2, ch.getToUsername());
-        ps.setInt(3, ch.getQuizId());
-        ps.executeUpdate();
+        try {
+            ps = this.conn.prepareStatement("INSERT INTO challenges(from_username, to_username, quiz_id) " +
+                    "VALUES(?, ?, ?)");
+            ps.setString(1, ch.getFromUsername());
+            ps.setString(2, ch.getToUsername());
+            ps.setInt(3, ch.getQuizId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void deleteChallenge(Challenge ch) throws SQLException {
+    public void deleteChallenge(Challenge ch) {
         PreparedStatement ps;
-        ps = this.conn.prepareStatement("DELETE FROM challenges WHERE " +
-                "from_username = ? AND to_username = ? AND quiz_id = ?");
-        ps.setString(1, ch.getFromUsername());
-        ps.setString(2, ch.getToUsername());
-        ps.setInt(3, ch.getQuizId());
-        ps.executeUpdate();
+        try {
+            ps = this.conn.prepareStatement("DELETE FROM challenges WHERE " +
+                    "from_username = ? AND to_username = ? AND quiz_id = ?");
+            ps.setString(1, ch.getFromUsername());
+            ps.setString(2, ch.getToUsername());
+            ps.setInt(3, ch.getQuizId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public List<Challenge> getChallenges(String username) throws SQLException {
+    public List<Challenge> getChallenges(String username) {
         PreparedStatement ps;
-        ps = this.conn.prepareStatement("SELECT * FROM challenges WHERE to_username = ?");
-        ps.setString(1, username);
-        ResultSet res = ps.executeQuery();
         List<Challenge> challenges = new ArrayList<Challenge>();
-        while (res.next()) {
-            int challengeId = res.getInt("id");
-            String fromUsername = res.getString("from_username");
-            String toUsername = res.getString("to_username");
-            int quizId = res.getInt("quiz_id");
-            Challenge ch = new Challenge(challengeId, fromUsername, toUsername, quizId);
-            challenges.add(ch);
+        try {
+            ps = this.conn.prepareStatement("SELECT * FROM challenges WHERE to_username = ?");
+            ps.setString(1, username);
+            ResultSet res = ps.executeQuery();
+            while (res.next()) {
+                int challengeId = res.getInt("id");
+                String fromUsername = res.getString("from_username");
+                String toUsername = res.getString("to_username");
+                int quizId = res.getInt("quiz_id");
+                Challenge ch = new Challenge(challengeId, fromUsername, toUsername, quizId);
+                challenges.add(ch);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return challenges;
     }
