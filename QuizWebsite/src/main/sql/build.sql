b5795ba17001b185b077ae6c631e660c3e30aaac
Administrator <administrator@morige-nb-08.ugt.ge>
2023-08-09 15:28:54 +0400

    Add quizzes and questions tables

--- QuizWebsite/src/main/sql/build.sql
+++ QuizWebsite/src/main/sql/build.sql
@@ -54,3 +54,27 @@ CREATE TABLE IF NOT EXISTS users
 
 ALTER TABLE users
     ADD CONSTRAINT unique_username UNIQUE (username);
+
+-- Creates the quizzes table. Contains information about quizzes:
+-- name, description, duration and other important details.
+-- There are quiz_id - primary key and author_id - foreign key that references
+-- user_id column of users table
+CREATE TABLE IF NOT EXISTS quizzes (
+    quiz_id INT AUTO_INCREMENT PRIMARY KEY,
+    quiz_name VARCHAR(100),
+    quiz_description VARCHAR(5000),
+    quiz_duration INT,
+    random_questions BOOLEAN,
+    multiple_pages BOOLEAN,
+    immediate_feedback BOOLEAN,
+    author_id int references users(user_id)
+);
+
+-- Creates the questions table. Contains information about questions.
+CREATE TABLE IF NOT EXISTS questions (
+    question_id INT AUTO_INCREMENT PRIMARY KEY,
+    question_content VARCHAR(5000),
+    question_type VARCHAR(100),
+    picture_url VARCHAR(200),
+    quiz_id INT REFERENCES quizzes (quiz_id) ON DELETE CASCADE
+);
